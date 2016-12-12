dc93662612d7ab1c389178c089689e9869f9954d
Rex Addiscentis <raddiscentis@addiscent.com>
2016-12-12 00:17:44 -0800

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # alert-bitcoin-address-change
+This Bash script detects a change in the amount of Bitcoin contained in a Bitcoin address and sends a notification which contains the new "total amount received" at the address, and the amount of the increase from the previous check.  When run periodically as a _cron_ job, it may serve as a "new Bitcoin payment" alert.
+
+This script creates persistent variable storage so the script will work when run as a cron job.  The variable storage file is created if it does not already exist.  The name of the file is the Bitcoin address.  Because there could be potentially many variable storage files, (one for each Bitcoin address), they are stored in a sub-directory of the user's $HOME direcotry, named "bitcoin_addrs".
+
+The source of the Bitcoin address related data is https://blockchain.info.
+
+This script has been tested only on Ubuntu 14.10 DESKTOP version.
+
+### Installation
+After cloning this project or downloading/unzipping the project zip file, copy _alert-bitcoin-address-change.sh_ into a directory in which the user has permissions, and ensure the file permission is set as _executable_.
+
+### Basic Command Line Syntax
+The Bitcoin address in this example below is specified as _1GdK9UzpHBzqzX2A9JFP3Di4weBwqgmoQA_.  Change this value to set the Bitcoin address which is tested for notification.  If email notification is desired, set the user's email address in place of the email address in the example below.  If no email notification is desired, remove the email address "example@example.com".  If "example@example.com" is not removed from the example below, the host will waste CPU and IO cycles pointlessly attempting to send unnecessary email, and email notifications of delivery failures may arrive in the host admin's mailbox.
+
+        alert-bitcoin-address-change.sh 1GdK9UzpHBzqzX2A9JFP3Di4weBwqgmoQA example@example.com
+
+### Used As A Buy Alert
+If the user wishes to regularly check by cron the amount of Bitcoin which the address has received, add the line of code shown below to the user's crontab .  The example line below causes the script to be executed once per minute.  Modify the example below accordingly if the script should be run less frequently.  The example code shown below assumes the user has alert-bitcoin-address-change.sh stored in a directory named "bin", in the "user" home directory. If stored in some other path, adjust this example accordingly.  
+
+        * * * * * /home/user/bin/alert-bitcoin-address-change.sh 1GdK9UzpHBzqzX2A9JFP3Di4weBwqgmoQA example@example.com >/dev/null 2>&1
+
+#### Important
+In order for email notifications to work correctly, the admin must ensure the _mail_ utility is installed and configured properly.
+
---
+++ alert-bitcoin-address-change.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# This program detects a change in the amount of Bitcoin contained in a
+# Bitcoin address and sends a notification containing the relevant values.
+#
+# This script creates persistent variable storage in a text file in the
+# $HOME dir so the script will work when run as a cron job.  The file
+# is created if it does not already exist.  Then name of the file is
+# the Bitcoin address.
+#
+# The source of the Bitcoin address related data is https://blockchain.info
+#
+# This script has been tested only on Ubuntu 14.10 DESKTOP version.
+#
+#
+# rex addiscentis  2016/12/11
+# Apache 2.0 License
+# https://github.com/addiscent
+
+
+# must specify a "bitcoin address" on command line
+if [ $# -lt 1 ]; then
+  echo "Usage   :  alert-bitcoin-address-change.sh  address  [email-address]"
+  echo "Example :  alert-bitcoin-address-change.sh  1GdK9UzpHBzqzX2A9JFP3Di4weBwqgmoQA  example@example.com"
+  echo "Alert \"Bitcoin address\" value required, exiting, nothing done"
+  exit 1
+fi 
+
+# fetch the json block containing current Bitcoin address data and
+# extract "total amount received" value
+latest_total_received=$(curl https://blockchain.info/address/$1?format=json | grep "total_received" | awk -F ',' '{print $1}' | cut -c22-32)
+echo "Fetched total_received Bitcoin amount is : $latest_total_received Satoshis"
+
+#set -x
+
+# if the previous total bitcoin received file does not exist create it and
+# set it to 0, after ensuring a directory exists for storing multiple
+# Bitcoin addresses
+bitcoin_addrs_dir="$HOME/bitcoin_addrs"
+previous_total_filename="$bitcoin_addrs_dir/$1"
+echo "previous_total_filename is :$previous_total_filename"
+if [ ! -f "$previous_total_filename"  ] ; then
+  if [ ! -d "$bitcoin_addrs_dir"  ] ; then
+    $(mkdir "$bitcoin_addrs_dir")
+  fi  
+  echo "0" > "$previous_total_filename"
+fi
+
+# fetch the previous_total_bitcoin_received from the variable storage file
+previous_total_bitcoin_received=$(cat "$previous_total_filename")
+echo "Previous Bitcoin total_received amount is : $previous_total_bitcoin_received Satoshis"
+
+# calc new Bitcoin amount difference.  If no difference, exit script
+let "new_difference = $latest_total_received - $previous_total_bitcoin_received"
+if [ $new_difference \= "0" ] ; then
+  echo "No New Bitcoin received, nothing to do"
+  #set +x
+  exit 0
+fi
+
+# trace values on STDOUT
+echo "New Bitcoin difference amount is : $new_difference Satoshis"
+echo "Bitcoin total_received amount is : $latest_total_received Satoshis"
+
+# notify statistics using GUI alert
+DISPLAY=:0 notify-send "Bitcoin Total Received Change" "Bitcoin Newly Received : $new_difference Satoshis,\nBitcoin Total Received : $latest_total_received Satoshis" 
+
+# if email address provided, send email notificiation
+if [ $# = 2 ] ; then
+  mail -s 'Bitcoin Total Received Change (Quasimodo)' "$2" << EOF
+From Bitcoin Address Monitor - Notification :
+
+   Bitcoin Newly Received : $new_difference Satoshis
+   Bitcoin Total Received : $latest_total_received Satoshis
+EOF
+fi
+
+# store variable for next run by cron
+echo "$latest_total_received" > "$previous_total_filename"
+
+#set +x
+
