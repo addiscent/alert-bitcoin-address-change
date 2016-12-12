14695f324565ac9cfe29574e9cbf0b407baf4930
Rex Addiscentis <raddiscentis@addiscent.com>
2016-12-12 11:36:17 -0800

    Detect bogus data, suspect service is unavailable

--- alert-bitcoin-address-change.sh
+++ alert-bitcoin-address-change.sh
@@ -25,11 +25,6 @@ if [ $# -lt 1 ]; then
   exit 1
 fi 
 
-# fetch the json block containing current Bitcoin address data and
-# extract "total amount received" value
-latest_total_received=$(curl https://blockchain.info/address/$1?format=json | grep "total_received" | awk -F ',' '{print $1}' | cut -c22-32)
-echo "Fetched total_received Bitcoin amount is : $latest_total_received Satoshis"
-
 #set -x
 
 # if the previous total bitcoin received file does not exist create it and
@@ -45,11 +40,36 @@ if [ ! -f "$previous_total_filename"  ] ; then
   echo "0" > "$previous_total_filename"
 fi
 
+# fetch the json block containing current Bitcoin address data and
+# extract "total amount received" value
+latest_total_received=$(curl https://blockchain.info/address/$1?format=json | grep "total_received" | awk -F ',' '{print $1}' | cut -c22-32)
+echo "Fetched total_received Bitcoin amount is : $latest_total_received Satoshis"
+
 # fetch the previous_total_bitcoin_received from the variable storage file
 previous_total_bitcoin_received=$(cat "$previous_total_filename")
 echo "Previous Bitcoin total_received amount is : $previous_total_bitcoin_received Satoshis"
 
-# calc new Bitcoin amount difference.  If no difference, exit script
+# https://blockchain.info service could have been unavailable during fetch.
+# Detect and exit if suspect
+if [ "$latest_total_received" \< "$previous_total_bitcoin_received" ] ; then
+  if [ $# = 2 ] ; then
+  mail -s 'Bitcoin Total Received Change (Quasimodo)' "$2" << EOF
+From Bitcoin Address Monitor - Notification :
+
+   Suspect data received from :
+     https://blockchain.info/address/$1?format=json
+   Service may be unavailable.
+   Bitcoin Total Received : $latest_total_received Satoshis
+   Bitcoin Previous Total Received : $previous_total_bitcoin_received Satoshis
+EOF
+  fi
+  echo "Suspect data received, service may be unavailable, exiting"
+  #set +x
+  exit 0
+fi
+
+# calc new Bitcoin amount difference.
+# if no difference, i.e., no new Bitcoin received, exit script
 let "new_difference = $latest_total_received - $previous_total_bitcoin_received"
 if [ $new_difference \= "0" ] ; then
   echo "No New Bitcoin received, nothing to do"
